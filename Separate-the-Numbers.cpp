acdcf4b20015e96f3a8d6fe6e9ab014aa09cc68a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-10-13 05:00:29 +0900

    Update Separate-the-Numbers.cpp

--- Separate-the-Numbers.cpp
+++ Separate-the-Numbers.cpp
@@ -1,1 +1,18 @@
+void separateNumbers(string s)
+{
+    std::string temp;
+    std::string num;
+    
+    for (int i = 1; i <= s.size()/2 && s != temp; ++i)
+    {
+        num = temp = s.substr(0, i);
+        
+        for (int j = 1; temp.size() < s.size(); ++j)
+        {
+            temp += to_string(stol(num) + j);
+        }
+    }
+
+    cout << (s == temp ? "YES " + num : "NO") << endl;
+}
 
