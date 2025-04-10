83d9af47f6cf8fb08a4bd7c79176387ec66fbd31
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-04-11 02:29:50 +0900

    Update Separate-the-Numbers.cpp

--- Separate-the-Numbers.cpp
+++ Separate-the-Numbers.cpp
@@ -29,3 +29,21 @@ void separateNumbers(string s)
     cout << (s == temp ? "YES " + num : "NO") << endl;
 }
 
+
+
+int main()
+{
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        separateNumbers(s);
+    }
+
+    return 0;
+}
