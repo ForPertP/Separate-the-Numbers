d490c93ba8b5f3680617a5e6a98ba29ff9a23345
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-04-18 04:10:04 +0900

    Update Separate-the-Numbers.cpp

--- Separate-the-Numbers.cpp
+++ Separate-the-Numbers.cpp
@@ -101,3 +101,12 @@ string ltrim(const string &str)
     return s;
 }
 
+
+string rtrim(const string &str)
+{
+    string s(str);
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
+    );
+    return s;
+}
