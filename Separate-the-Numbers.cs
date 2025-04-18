ff860426cfc9aa04bd36b12dca3d02371292f5a6
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-04-19 04:24:13 +0900

    Update Separate-the-Numbers.cs

--- Separate-the-Numbers.cs
+++ Separate-the-Numbers.cs
@@ -1,1 +1,24 @@
 Separate-the-Numbers
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'separateNumbers' function below.
+     *
+     * The function accepts STRING s as parameter.
+     */
+}
