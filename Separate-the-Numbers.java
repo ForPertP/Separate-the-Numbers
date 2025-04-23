9c0c995ae879270bde2fa3463e8996a39802d07e
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-04-24 04:17:38 +0900

    Update Separate-the-Numbers.java

--- Separate-the-Numbers.java
+++ Separate-the-Numbers.java
@@ -1,1 +1,22 @@
-Separate-the-Numbers
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                Result.separateNumbers(s);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+}
