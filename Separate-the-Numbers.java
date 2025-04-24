344abd6f1edcac83fac312c67102d003d2c83608
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-04-25 03:31:08 +0900

    Update Separate-the-Numbers.java

--- Separate-the-Numbers.java
+++ Separate-the-Numbers.java
@@ -1,4 +1,24 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
+class Result {
+
+    /*
+     * Complete the 'separateNumbers' function below.
+     *
+     * The function accepts STRING s as parameter.
+     */
+
+}
 
 
 public class Solution {
